6fa97411e4beb24d16e42b85004977f0bb440562
David Berenstein <david.m.berenstein@gmail.com>
2024-04-03 13:36:24 +0200

    Update refresh.py

--- refresh.py
+++ refresh.py
@@ -16,11 +16,11 @@ rg.init(
 )
 
 def completed_with_overlap(records, min_overlap=2):
-  completed = []
-  for r in records:
-      if len(r.responses)>=2:
-        completed.append(r)
-  return completed
+    completed = []
+    for r in records:
+        if len(r.responses)>=2:
+            completed.append(r)
+    return completed
 
 dataset = rg.FeedbackDataset.from_argilla(SOURCE_DATASET, workspace=SOURCE_WS)
 print(f"Current dataset size: {len(dataset)}")
@@ -32,14 +32,13 @@ completed_remote_records = completed_with_overlap(submitted_so_far.records)
 print(f"Completed so far: {len(completed_remote_records)}")
 
 if len(completed_remote_records)>0:
-  local_submitted = submitted_so_far.pull()
-  completed_local_records = completed_with_overlap(local_submitted.records)
-  try:
-    print(f"Updating private results with {len(completed_remote_records)} records.")
-    results = rg.FeedbackDataset.from_argilla(RESULTS_DATASET, workspace=RESULTS_WS)
-    results.add_records(completed_local_records)
-    results.push_to_huggingface("argilla/prompt-collective", token=os.getenv("HF_TOKEN"))
-  except:
-    local_submitted.push_to_argilla(RESULTS_DATASET, workspace=RESULTS_WS)
-
-  dataset.delete_records(list(completed_remote_records))
+    local_submitted = submitted_so_far.pull()
+    completed_local_records = completed_with_overlap(local_submitted.records)
+    try:
+        print(f"Updating private results with {len(completed_remote_records)} records.")
+        results = rg.FeedbackDataset.from_argilla(RESULTS_DATASET, workspace=RESULTS_WS)
+        results.add_records(completed_local_records)
+        results.push_to_huggingface("argilla/prompt-collective", token=os.getenv("HF_TOKEN"))
+    except:
+        local_submitted.push_to_argilla(RESULTS_DATASET, workspace=RESULTS_WS)
+    dataset.delete_records(list(completed_remote_records))

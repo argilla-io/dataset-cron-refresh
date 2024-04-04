51f42c5d6d28631bba36629c1b3c4fa618f7a3f5
davidberenstein1957 <david.m.berenstein@gmail.com>
2024-04-04 10:59:27 +0200

    Add examples to GH

---
+++ main.py
@@ -0,0 +1,70 @@
+import os
+
+import argilla as rg
+
+REQUIRED_RESPONSES = os.environ["REQUIRED_RESPONSES"]
+HF_TOKEN = os.environ["HF_TOKEN"]
+languages = ["DUTCH"]
+
+dataset_info = [
+    (
+        language_,
+        os.environ[f"ARGILLA_API_URL_{language_}"],
+        os.environ[f"ARGILLA_API_KEY_{language_}"],
+        os.environ[f"SOURCE_DATASET_{language_}"],
+        os.environ[f"SOURCE_WORKSPACE_{language_}"],
+        os.environ[f"RESULTS_DATASET_{language_}"],
+        os.environ[f"RESULTS_WORKSPACE_{language_}"],
+    )
+    for language_ in languages
+]
+
+
+def completed_with_overlap(records, required_responses):
+    completed = []
+    for r in records:
+        if len(r.responses) >= required_responses:
+            completed.append(r)
+    return completed
+
+
+for (
+    language,
+    api_url,
+    api_key,
+    SOURCE_DATASET,
+    SOURCE_WS,
+    RESULTS_DATASET,
+    RESULTS_WS,
+) in dataset_info:
+    rg.init(api_url=api_url, api_key=api_key)
+    dataset = rg.FeedbackDataset.from_argilla(SOURCE_DATASET, workspace=SOURCE_WS)
+    print(f"Current dataset size {language}: {len(dataset)} ")
+
+    submitted_so_far = dataset.filter_by(response_status="submitted")
+    print(f"Submitted {language}: {len(submitted_so_far)}")
+
+    completed_remote_records = completed_with_overlap(
+        submitted_so_far.records, REQUIRED_RESPONSES
+    )
+    print(f"Completed so far {language}: {len(completed_remote_records)}")
+
+    if len(completed_remote_records) > 0:
+        local_submitted = submitted_so_far.pull()
+        completed_local_records = completed_with_overlap(
+            local_submitted.records, REQUIRED_RESPONSES
+        )
+        try:
+            print(
+                f"Updating private results with {len(completed_remote_records)} records {language}."
+            )
+            results = rg.FeedbackDataset.from_argilla(
+                RESULTS_DATASET, workspace=RESULTS_WS
+            )
+            results.add_records(completed_local_records)
+            results.push_to_huggingface(
+                "dibt/prompt-collective", token=os.getenv("HF_TOKEN")
+            )
+        except Exception as _:
+            local_submitted.push_to_argilla(RESULTS_DATASET, workspace=RESULTS_WS)
+        dataset.delete_records(list(completed_remote_records))

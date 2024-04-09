3619988c511dee9cac93ebaf42fc7db6183280af
David Berenstein <david.m.berenstein@gmail.com>
2024-04-09 17:25:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import argilla as rg
 
 REQUIRED_RESPONSES = int(os.environ["REQUIRED_RESPONSES"])
 HF_TOKEN = os.environ["HF_TOKEN"]
-languages = ["DUTCH", "SPANISH", "MALAGASY", "GERMAN", "SWAHILI"]
+languages = ["DUTCH", "SPANISH", "MALAGASY", "GERMAN", "SWAHILI", "FILIPINO", "ARABIC"]
 
 dataset_info = [
     (

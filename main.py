3e9568c6dfc5633ecec513357f23a7c9f9d28549
ignacioct <ignaciotalaveracepeda@gmail.com>
2024-05-15 10:04:42 +0200

    added exception if the dataset is not found

--- main.py
+++ main.py
@@ -59,38 +59,43 @@ for (
     if not workspace_presnt:
         rg.Workspace.create(RESULTS_WS)
 
-    dataset = rg.FeedbackDataset.from_argilla(SOURCE_DATASET, workspace=SOURCE_WS)
-    print(f"Current dataset size {language}: {len(dataset)} ")
+    try:
+        dataset = rg.FeedbackDataset.from_argilla(SOURCE_DATASET, workspace=SOURCE_WS)
+        print(f"Current dataset size {language}: {len(dataset)} ")
 
-    submitted_so_far = dataset.filter_by(response_status="submitted")
-    print(f"Submitted {language}: {len(submitted_so_far)}")
+        submitted_so_far = dataset.filter_by(response_status="submitted")
+        print(f"Submitted {language}: {len(submitted_so_far)}")
 
-    completed_remote_records = completed_with_overlap(
-        submitted_so_far.records, REQUIRED_RESPONSES
-    )
-    print(f"Completed so far {language}: {len(completed_remote_records)}")
-
-    if len(completed_remote_records) > 0:
-        local_submitted = submitted_so_far.pull()
-        completed_local_records = completed_with_overlap(
-            local_submitted.records, REQUIRED_RESPONSES
+        completed_remote_records = completed_with_overlap(
+            submitted_so_far.records, REQUIRED_RESPONSES
         )
-        try:
-            results = rg.FeedbackDataset.from_argilla(
-                RESULTS_DATASET, workspace=RESULTS_WS
+        print(f"Completed so far {language}: {len(completed_remote_records)}")
+
+        if len(completed_remote_records) > 0:
+            local_submitted = submitted_so_far.pull()
+            completed_local_records = completed_with_overlap(
+                local_submitted.records, REQUIRED_RESPONSES
             )
             try:
-                results.add_records(completed_local_records)
-                dataset.delete_records(list(completed_remote_records))
+                results = rg.FeedbackDataset.from_argilla(
+                    RESULTS_DATASET, workspace=RESULTS_WS
+                )
+                try:
+                    results.add_records(completed_local_records)
+                    dataset.delete_records(list(completed_remote_records))
+                except Exception as e:
+                    pass
             except Exception as e:
-                pass
-        except Exception as e:
-            results = local_submitted.push_to_argilla(
-                RESULTS_DATASET, workspace=RESULTS_WS
-            )
-            dataset.delete_records(list(completed_remote_records))
+                results = local_submitted.push_to_argilla(
+                    RESULTS_DATASET, workspace=RESULTS_WS
+                )
+                dataset.delete_records(list(completed_remote_records))
 
-        print(
-            f"Updating private results with {len(completed_remote_records)} records {language}."
-        )
-        results.push_to_huggingface(f"DIBT/MPEP_{language}", token=HF_TOKEN)
+            print(
+                f"Updating private results with {len(completed_remote_records)} records {language}."
+            )
+            results.push_to_huggingface(f"DIBT/MPEP_{language}", token=HF_TOKEN)
+    
+    except ValueError as e:
+        print(f"Dataset {SOURCE_DATASET} not found.\nError: {e}")
+        continue
